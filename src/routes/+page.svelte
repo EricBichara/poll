bc51e36b3e33cec0f3b21ee03ae22719cca53480
ericb <eric.bichara@gmail.com>
2022-09-11 12:18:48 +0200

    random

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,53 @@
 <script lang="ts">
-    import {Dropdown, Theme} from "carbon-components-svelte";
+    import {PieChart} from "@carbon/charts-svelte";
+    import "@carbon/styles/css/styles.css";
+    import "@carbon/charts/styles.css";
 
-
-    let selectedId = 3;
+    let selectedId = 2;
 
     let availableThemes = [{id: 0, text: 'white'}, {id: 1, text: 'g10'}, {id: 2, text: 'g80'}, {
         id: 3,
         text: 'g90'
     }, {id: 4, text: 'g100'}]
 
-    function getTheme(themeId){
+    function getTheme(themeId) {
         const thetheme = availableThemes.find(item => item.id === themeId);
         return thetheme.text;
     }
 
+    function updateChart() {
+        data[1].value += 10000;
+        data = data;
+    }
+
     $: theme = getTheme(selectedId);
+
+    let data = [
+        {
+            "group": "2V2N 9KYPM version 1",
+            "value": 20000,
+        },
+        {
+            "group": "L22I P66EP L22I P66EP L22I P66EP",
+            "value": 85000
+        },
+        {
+            "group": "JQAI 2M4L1",
+            "value": 75000
+        },
+        {
+            "group": "J9DZ F37AP",
+            "value": 100200
+        },
+        {
+            "group": "YEL48 Q6XK YEL48",
+            "value": 10000
+        },
+        {
+            "group": "Misc",
+            "value": 25000
+        }
+    ];
 </script>
 
 <svelte:head>
@@ -22,10 +55,6 @@
     <meta name="description" content="Svelte demo app"/>
 </svelte:head>
 
-<Theme bind:theme></Theme>
-
-<Dropdown
-        titleText="Theme"
-        selected="0"
-        bind:selectedId={selectedId}
-        items={availableThemes}></Dropdown>
+<PieChart
+        data={data}
+        options={{"title":"Pie", "height": "400px"}}></PieChart>
---
+++ src/routes/random/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+
+    import {Button} from "carbon-components-svelte";
+
+    let key = "E";
+
+    let type = "Minor"
+
+    let intervalId;
+
+    const keys = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    const types = ['Major', 'Minor'];
+
+    function startInterval() {
+        stopInterval();
+        intervalId = setInterval(doAtInterval, 10000);
+    }
+
+    function stopInterval(){
+        if(intervalId){
+            clearInterval(intervalId);
+        }
+    }
+
+    function doAtInterval() {
+        key = keys[Math.floor(Math.random() * keys.length)];
+        type = types[Math.floor(Math.random()*types.length)];
+    }
+</script>
+
+<style>
+    div {
+        font-size: 60px;
+        text-align: center;
+        margin-bottom: 30px;
+    }
+</style>
+
+<div>{key}</div>
+<div>{type}</div>
+
+<div>
+    <Button on:click={startInterval}>Start</Button>
+    <Button on:click={stopInterval}>Stop</Button>
+    <Button on:click={doAtInterval}>Next</Button>
+</div>
